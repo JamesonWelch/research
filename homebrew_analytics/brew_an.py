1915a7c74031852e939483bbdf737153abd6cb35
Jameson <48740182+JamesonWelch@users.noreply.github.com>
2019-06-21 15:22:53 -0700

    Added analytics selection functionality

--- homebrew_analytics/brew_an.py
+++ homebrew_analytics/brew_an.py
@@ -1,14 +1,24 @@
 
 import json
 
+print('Select data point to sort by: ')
+sel = str(input('\'a\' for 30 days\n\'b\' for 90 days\n\'c\' for 365 days: '))
+sel = sel.lower()
+if sel == 'a':
+    day = '30d'
+elif sel == 'b':
+    day = '90d'
+elif sel == 'c':
+    day = '365d'
+
 def install_sort(package):
-    return package['analytics']['365d']
+    return package['analytics'][day]
 
 with open('package_info.json', 'r') as f:
     data = json.load(f)
 
 # Sort by description
-data = [item for item in data if 'video' in item['desc']]
+# data = [item for item in data if 'video' in item['desc']]
 data.sort(key=install_sort, reverse=True)
 
 data_str = json.dumps(data[:5], indent=2)
