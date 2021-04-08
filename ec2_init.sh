4d3ef31dd0e4ae3c778075d59df64f2ce7945831
JamesonWelch <jamesonb.welch@gmail.com>
2021-04-08 11:41:58 -0700

    Added apt and yum

--- ec2_init.sh
+++ ec2_init.sh
@@ -22,11 +22,26 @@ sudo pip3 install virtualenv || sudo apt install virtualenv || sudo yum install 
 mkdir dev
 mkdir prod
 cd prod
-mkdir tkdb
+mkdir tkpy
 
 virtualenv --python=python3 venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt || echo "requirements.txt pip install failed, check to see file exists"
+
+python3 -m pip install async-timeout
+python3 -m pip install aiohttp
+python3 -m pip install selenium
+python3 -m pip install bs4
+python3 -m pip install --upgrade pandas
+python3 -m pip install --upgrade snowflake-sqlalchemy
+python3 -m pip install simple-salesforce
+python3 -m pip uninstall requests -y
+python3 -m pip install requests==2.22.0
+python3 -m pip install pybigquery
+python3 -m pip install numpy (needs verification)
+python3 -m pip install --upgrade snowflake-connector-python
+python3 -m pip install snowflake-connector-python[pandas]
+python3 -m pip install --upgrade parquet
 
 google-chrome --version && which google-chrome
 python --version && which python
