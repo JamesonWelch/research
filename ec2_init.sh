a159662f7528eeb1078d7064bf2b5eab66c2136f
JamesonWelch <jamesonb.welch@gmail.com>
2021-04-08 11:15:26 -0700

    Improved functionality

--- ec2_init.sh
+++ ec2_init.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # update
-sudo apt update
+sudo apt update || sudo yum update
+
+# install Python and dependencies
+sudo apt install python3 || sudo yum install python3
+pip3 --version || sudo apt install python3-pip || sudo yum install python3-pip
+sudo pip3 install virtualenv || sudo apt install virtualenv || sudo yum install virtualenv
+
+# Project-specific commands
+mkdir dev
+mkdir prod
+cd prod
+mkdir tkdb
+
+virtualenv --python=python3 venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt || echo "requirements.txt pip install failed, check to see file exists"
 
 # Install Selenium webdriver and chrome
 wget https://chromedriver.storage.googleapis.com/89.0.438.23/chromedriver_linux64.zip
@@ -13,18 +28,3 @@ chromedriver --version
 curl https://intoli.com/install-google-chrome.sh | bash || curl https://intoli.com/install-google-chrome.sh
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
 google-chrome --version && which google-chrome
-
-# install Python and dependencies
-sudo apt install python3
-pip3 --version || sudo apt install python3-pip
-sudo pip3 install virtualenv || sudo apt install virtualenv
-
-# Project-specific commands
-mkdir dev
-mkdir prod
-cd prod
-mkdir tkdb
-
-virtualenv --python=python3 venv
-source venv/bin/activate
-python3 -m pip install -r requirements.txt || echo "requirements.txt pip install failed, check to see file exists"
