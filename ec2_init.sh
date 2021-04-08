7d69af2a66ac76bdb5401246cef586a8133aac4b
JamesonWelch <jamesonb.welch@gmail.com>
2021-04-08 15:15:15 -0700

    Added venv separation file

--- ec2_init.sh
+++ ec2_init.sh
@@ -21,27 +21,27 @@ sudo pip3 install virtualenv || sudo apt install virtualenv || sudo yum install 
 # Project-specific commands
 mkdir dev
 mkdir prod
-cd prod
-mkdir tkpy
+mkdir prod/tkpy
 
-virtualenv --python=python3 venv
-source venv/bin/activate
-python3 -m pip install -r requirements.txt || echo "requirements.txt pip install failed, check to see file exists"
+# virtualenv --python=python3 venv
+# source venv/bin/activate
+# python3 -m pip install -r requirements.txt || echo "requirements.txt pip install failed, check to see file exists"
 
-python3 -m pip install async-timeout
-python3 -m pip install aiohttp
-python3 -m pip install selenium
-python3 -m pip install bs4
-python3 -m pip install --upgrade pandas
-python3 -m pip install --upgrade snowflake-sqlalchemy
-python3 -m pip install simple-salesforce
-python3 -m pip uninstall requests -y
-python3 -m pip install requests==2.22.0
-python3 -m pip install pybigquery
-python3 -m pip install numpy (needs verification)
-python3 -m pip install --upgrade snowflake-connector-python
-python3 -m pip install snowflake-connector-python[pandas]
-python3 -m pip install --upgrade parquet
+# python3 -m pip install async-timeout
+# python3 -m pip install aiohttp
+# python3 -m pip install selenium
+# python3 -m pip install progress
+# python3 -m pip install bs4
+# python3 -m pip install --upgrade pandas
+# python3 -m pip install --upgrade snowflake-sqlalchemy
+# python3 -m pip install simple-salesforce
+# python3 -m pip uninstall requests -y
+# python3 -m pip install requests==2.22.0
+# python3 -m pip install pybigquery
+# python3 -m pip install numpy (needs verification)
+# python3 -m pip install --upgrade snowflake-connector-python
+# python3 -m pip install snowflake-connector-python[pandas]
+# python3 -m pip install --upgrade parquet
 
 google-chrome --version && which google-chrome
 python --version && which python
---
+++ ec2_init2.sh
@@ -0,0 +1,15 @@
+python3 -m pip install async-timeout
+python3 -m pip install aiohttp
+python3 -m pip install selenium
+python3 -m pip install progress
+python3 -m pip install bs4
+python3 -m pip install --upgrade pandas
+python3 -m pip install --upgrade snowflake-sqlalchemy
+python3 -m pip install simple-salesforce
+python3 -m pip uninstall requests -y
+python3 -m pip install requests==2.22.0
+python3 -m pip install pybigquery
+python3 -m pip install numpy (needs verification)
+python3 -m pip install --upgrade snowflake-connector-python
+python3 -m pip install snowflake-connector-python[pandas]
+python3 -m pip install --upgrade parquet
